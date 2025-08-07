ee840804cc99de3f313f82144377e66ee14adbbe
Alex Mendonca <alex.mendonca@consensys.net>
2025-08-07 10:59:20 +0100

    match test-dapp-multichain config

---
+++ .eslintrc.cjs
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+
+  extends: ['@metamask/eslint-config'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        'import/no-unassigned-import': 'off',
+        'import/extensions': 'off',
+        'jsdoc/require-jsdoc': 'off',
+        'no-restricted-globals': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+
+    {
+      files: ['*.js', '*.cjs'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+
+    {
+      files: ['vite.config.ts', 'tailwind.config.js', 'postcss.config.js'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+      rules: {
+        'import/unambiguous': 'off',
+      },
+    },
+
+    {
+      files: ['src/vite-env.d.ts'],
+      rules: {
+        'import/unambiguous': 'off',
+      },
+    },
+
+    {
+      files: ['*.test.ts', '*.test.js', '*.test.tsx'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+      },
+    },
+  ],
+
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    '!.prettierrc.js',
+    'build/',
+    'dist/',
+    'docs/',
+    '.yarn/',
+    'node_modules/',
+  ],
+};
