2523378cf659dab994ece79d52db0f770b6a90a6
Alex Mendonca <alex.mendonca@consensys.net>
2025-08-07 10:51:21 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-center text-gray-900">
+          MetaMask Pay DApp
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
