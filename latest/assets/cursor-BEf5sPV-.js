ab73f0b0392acda13f78eb6981ba168f65499083
wenfix <wenfix@users.noreply.github.com>
2025-09-04 18:12:50 +0000

    deploy: f5fc765a2f073697a92e923827af197b22115d2d

--- latest/assets/cursor-BEf5sPV-.js
+++
@@ -1,3 +0,0 @@
-import{a5 as o}from"./ThemeUtil-Ns1L8aKl.js";import"./index-CBCzWjyo.js";const i=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{i as cursorSvg};
