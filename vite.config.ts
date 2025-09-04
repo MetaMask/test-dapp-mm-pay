dba722f03056fd7b511c542be5fec51088a0148a
aphex <52055541+wenfix@users.noreply.github.com>
2025-09-04 19:00:57 +0100

    fix: dynamic base path (#8)

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+import { homepage } from './package.json';
+
+// eslint-disable-next-line require-unicode-regexp
+const basePath = homepage.replace(/^https?:\/\/[^/]+/, '');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [nodePolyfills(), react()],
-  base: process.env.NODE_ENV === 'production' ? '/metamask-pay-dapp/' : '/',
+  base: process.env.NODE_ENV === 'production' ? basePath : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
