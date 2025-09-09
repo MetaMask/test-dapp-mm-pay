ccdd6884fbcb0ddd36889c5cd2ed4e3679a09e70
wenfix <wenfix@users.noreply.github.com>
2025-09-09 16:30:39 +0000

    deploy: 74b3fd5f12633e298d26da9c43fb40ac1255eacd

---
+++ latest/assets/chevron-right-BiGKXF2E.js
@@ -0,0 +1,8 @@
+import{a5 as l}from"./ThemeUtil-BizdN5P6.js";import"./index-De8h_K6m.js";const r=l`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{r as chevronRightSvg};
