832e41bdb79af047a8cf1b464405c8f268118013
Alex Mendonca <alex.mendonca@consensys.net>
2026-04-13 14:47:53 +0100

    fix margin

--- src/AppMmPay.tsx
+++ src/AppMmPay.tsx
@@ -45,7 +45,7 @@ function MainContent() {
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
-        className="rounded-xl border border-pay-border bg-pay-surface/50 p-4"
+        className="mb-4 rounded-xl border border-pay-border bg-pay-surface/50 p-4"
       >
         <ActionSelector
           selectedAction={selectedAction}
